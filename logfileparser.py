54f1d8fca638f3af3377daf0d452f0dcc3a9ba67
bonfirepilot <60601856+bonfirepilot@users.noreply.github.com>
2020-03-02 11:29:09 -0600

    Update logfileparser.py
    Added Timestamp Parser, Redirect and Error Counter, as well as log subdivision by month

--- logfileparser.py
+++ logfileparser.py
@@ -30,19 +30,18 @@ for line in lineRead:
         
 #Month Breakdown
 months_count = {"Jan": 0, "Feb": 0, "Mar": 0, "Apr": 0, "May": 0, "Jun": 0, "Jul": 0, "Aug": 0, "Sep": 0, "Oct": 0, "Nov": 0, "Dec": 0}
-
-janlogs = open("janLog.txt", "a+");
-feblogs = open("febLog.txt", "a+");
-marlogs = open("marLog.txt", "a+");
-aprlogs = open("aprLog.txt", "a+");
-maylogs = open("mayLog.txt", "a+");
-junlogs = open("junLog.txt", "a+");
-jullogs = open("julLog.txt", "a+");
-auglogs = open("augLog.txt", "a+");
-seplogs = open("sepLog.txt", "a+");
-octlogs = open("octLog.txt", "a+");
-novlogs = open("novLog.txt", "a+");
-declogs = open("decLog.txt", "a+");
+jan_logs = open("janLog.txt", "a+");
+feb_logs = open("febLog.txt", "a+");
+mar_logs = open("marLog.txt", "a+");
+apr_logs = open("aprLog.txt", "a+");
+may_logs = open("mayLog.txt", "a+");
+jun_logs = open("junLog.txt", "a+");
+jul_logs = open("julLog.txt", "a+");
+aug_logs = open("augLog.txt", "a+");
+sep_logs = open("sepLog.txt", "a+");
+oct_logs = open("octLog.txt", "a+");
+nov_logs = open("novLog.txt", "a+");
+dec_logs = open("decLog.txt", "a+");
 
 #Setting up File Length Counter
 def file_len(LOCAL_FILE):
@@ -62,7 +61,33 @@ def fileCounter():
             except:
                 pass
     counter = collections.Counter(filelog)
-    #Most Commonly Requested File Finder
+ #Most Commonly Requested File Finder
     for count in counter.most_common(1):
         print("Most commonly requested file: {} with {} requests.".format(str(count[0]), str(count[1])))
+        
+ #Timestamp Parser for Log Subdivision
+    timestamp = match.group(3)
+    month = timestamp[3:6]
+    months_count[month] += 1
+    match.group(7)
+ #ReDirect Counter, Error Counter, and Log Subdivider
+    if (match.group(7)[0] == "3"):
+        redirectCounter += 1
+    elif (match.group(7)[0] == "4"):
+        errorCounter += 1
+    if (month == "Jan"):jan_logs.write(line)
+    elif (month == "Feb"):feb_logs.write(line)
+    elif (month == "Mar"):mar_logs.write(line)
+    elif (month == "Apr"):apr_logs.write(line)
+    elif (month == "May"):may_logs.write(line)
+    elif (month == "Jun"):jun_logs.write(line)
+    elif (month == "Jul"):jul_logs.write(line)
+    elif (month == "Aug"):aug_logs.write(line)
+    elif (month == "Sep"):sep_logs.write(line)
+    elif (month == "Oct"):oct_logs.write(line)
+    elif (month == "Nov"):nov_logs.write(line)
+    elif (month == "Dec"):dec_logs.write(line)
+
+    else:
+        continue
 
